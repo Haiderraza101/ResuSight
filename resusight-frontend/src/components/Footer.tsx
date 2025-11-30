212a2384c9e9b25fd1d40e713f5e5184e1b81791
Haider <haiderraza7516@gmail.com>
2025-11-30 05:50:11 +0500

    minor change~c

--- resusight-frontend/src/components/Footer.tsx
+++ resusight-frontend/src/components/Footer.tsx
@@ -160,7 +160,7 @@ const Footer: React.FC = () => {
 
         <div className="border-t border-gray-800 pt-8 flex flex-col md:flex-row justify-between items-center">
           <div className="text-gray-500 mb-4 md:mb-0">
-            <p>&copy; 2024 ResuSight. All rights reserved.</p>
+            <p>&copy; 2025 ResuSight. All rights reserved.</p>
           </div>
           <div className="flex gap-6">
             <a
